4a984bc40c97c680d57c12ff853f2abf7b0ff9e7
Simo Soininen <soinisi@gmail.com>
2021-01-31 22:45:23 +0200

    added cmd line json arg

--- RFServer/rfserver_start.py
+++ RFServer/rfserver_start.py
@@ -1,22 +1,25 @@
-import sys
+import sys, os
 import json
 from robot.api import TestSuite
 from robot.api import logger
 from RFServer.server_interface import RFServerInterface
 from dateutil import parser
-
+from distutils.util import strtobool
 
 class RFServerStart(RFServerInterface):
     
-    def get_keyword_request(self, json_path: str) -> dict:
-        
-        with open(json_path) as json_file:
-            kw_dict = json.load(json_file)
+    def get_keyword_request(self, json_item: str) -> dict:
+        try:
+            kw_dict = json.loads(json_item)
+            
+        except Exception:
+            with open(json_item) as json_file:
+                kw_dict = json.load(json_file)
         
         kw_dict['expiration'] = parser.parse(kw_dict['expiration'])
         
         if 'exit' in kw_dict:
-            kw_dict['exit'] = eval(kw_dict['exit'].strip().capitalize())
+            kw_dict['exit'] = bool(strtobool(kw_dict['exit'].strip()))
     
         return kw_dict
 
