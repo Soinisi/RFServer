c565de7a254021cdb8f9fccdcce5db465d180026
Simo Soininen <soinisi@gmail.com>
2022-07-29 22:45:45 +0300

    expanded server data testing

--- RFServer/tests/test_data_validator.py
+++ RFServer/tests/test_data_validator.py
@@ -3,15 +3,25 @@ from RFServer.data_validator import validate_server_schema, validate_config_sche
 from datetime import datetime
 from schema import SchemaError, SchemaMissingKeyError
 
-def test_server_data_validation():
-    succ_server_data = {'keyword':'test kw', 'sender_id':'test1234', 'expiration': datetime(9999, 1, 1)}
-    fail_server_data = {'keyword':'test kw'}
+def test_server_data_validation_success():
+    succ_no_exit_data = {'keyword':'test kw', 'sender_id':'test1234', 'expiration': datetime(9999, 1, 1)}
+    succ_no_keyword_data = {'exit': True, 'sender_id':'test1234', 'expiration': datetime(9999, 1, 1)}
 
-    validate_server_schema(succ_server_data)
+    assert validate_server_schema(succ_no_exit_data)
+    assert validate_server_schema(succ_no_keyword_data)
+
+
+def test_server_data_validation_fail():
+    fail_mandatory_data = {'keyword':'test kw'}
+    fail_no_actions_data = {'sender_id':'test1234', 'expiration': datetime(9999, 1, 1)}
+
     with pytest.raises(SchemaMissingKeyError) as exc_info:
-        validate_server_schema(fail_server_data)
-
+        validate_server_schema(fail_mandatory_data)
     assert 'Missing keys:' in str(exc_info)
+
+    with pytest.raises(SchemaMissingKeyError) as exc_info:
+        validate_server_schema(fail_no_actions_data)
+    assert 'Missing key:' in str(exc_info)
 
 
 def test_config_data_validation():
