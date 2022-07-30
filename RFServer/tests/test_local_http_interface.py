59e21a0bc9181da60981d5fa0b35fecb54d76d62
Simo Soininen <soinisi@gmail.com>
2022-07-30 23:31:47 +0300

    refactor to http_interface tests

--- RFServer/tests/test_local_http_interface.py
+++ RFServer/tests/test_local_http_interface.py
@@ -4,16 +4,38 @@ from pathlib import Path
 import requests
 from requests.exceptions import ConnectionError
 import pytest
+import time
 
 LOCAL_HTTP_SERVER_ADDRESS = 'http://localhost:8000/keyword'
 
-def test_run_keyword_get_return():
+server_thread = None
+
+def setup_function():
     server_path = str(Path(__file__).parent / 'res' / 'http_test_conf.yaml')
     server_cmd = 'rf_server ' + server_path
+
+    global server_thread
     server_thread = Thread(target = os.system, args = (server_cmd,))
     server_thread.start()
     server_thread.join(5)
 
+def teardown_function():
+    global server_thread
+    time_count = 0
+    incr = 0.1
+    while server_thread.is_alive():
+        time_count += incr
+        if time_count > 10:
+            raise TimeoutError('server running after max shutdown time')
+        time.sleep(incr)
+
+    with pytest.raises(ConnectionError) as exc_info:
+        requests.post(LOCAL_HTTP_SERVER_ADDRESS, json = {})
+
+    assert 'Failed to establish a new connection' in str(exc_info)
+
+
+def test_run_keyword_get_return():
     kw_req_dict = {"expiration":"18.09.2099",
                 "sender_id":"123",
                 "keyword":"convert to string",
@@ -26,13 +48,9 @@ def test_run_keyword_get_return():
     assert kw_answ_dict['kw_status'] == 'pass'
     assert kw_answ_dict['return_value'] == '100'
 
-def test_run_keyword_get_error():
-    server_path = str(Path(__file__).parent / 'res' / 'http_test_conf.yaml')
-    server_cmd = 'rf_server ' + server_path
-    server_thread = Thread(target = os.system, args = (server_cmd,))
-    server_thread.start()
-    server_thread.join(5)
 
+
+def test_run_keyword_get_error():
     kw_req_dict = {"expiration":"18.09.2099",
                 "sender_id":"234",
                 "keyword":"random keyword",
@@ -46,14 +64,8 @@ def test_run_keyword_get_error():
     assert kw_answ_dict['kw_status'] == 'fail'
     assert kw_answ_dict['return_value'] == "No keyword with name 'random keyword' found."
 
+
 def test_run_exit():
-    server_path = str(Path(__file__).parent / 'res' / 'http_test_conf.yaml')
-    server_cmd = 'rf_server ' + server_path
-
-    server_thread = Thread(target = os.system, args = (server_cmd,))
-    server_thread.start()
-    server_thread.join(5)
-
     kw_req_dict = {"expiration":"18.09.2099",
                 "sender_id":"345",
                 "exit":"True"}
@@ -61,7 +73,3 @@ def test_run_exit():
     res = requests.post(LOCAL_HTTP_SERVER_ADDRESS, json = kw_req_dict)
     kw_answ_dict = res.json()
     assert kw_answ_dict['sender_id'] == '345'
-    with pytest.raises(ConnectionError) as exc_info:
-        requests.post(LOCAL_HTTP_SERVER_ADDRESS, json = kw_req_dict)
-
-    assert 'Connection aborted.' in str(exc_info)
